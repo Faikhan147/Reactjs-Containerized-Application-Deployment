4b40bb8bf1c587291fd0d8e25e2be0beaa533be7
Faisal Khan <faisalkhanattari35@gmail.com>
2025-09-13 03:03:34 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,28 @@
+# Update the packages
+sudo apt update
+sudo apt install -y unzip
+
+# Install Docker (only if needed for local builds)
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
+docker --version  # Verify Docker version
+
+
+
+# Download AWS CLI v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+aws --version
+
+
+
+# Install kubectl
+KUBECTL_VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+curl -LO "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+kubectl version --client  # Verify kubectl version
